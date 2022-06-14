c1484b4cf4018be77c696c5f857048e436e65cf6
pegaatron <ppt003@ucsd.edu>
2022-06-14 15:41:02 -0700

    added req.query to GetQuestions for Loader.io tesing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // get all questions
 app.get('/qa/questions', (req, res) => {
-  var a = req.body.product_id || '1000010';
+  var a = req.body.product_id || req.query.product_id || '1000010';
   models.getQuestions([a])
   .then((data) => {
     res.send(data.rows[0]).status(200)
